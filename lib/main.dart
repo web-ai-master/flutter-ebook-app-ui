a4f00595e1b779e9496b8a00eb2bc4c203a1a069
jovan0901 <jovanvelickovic0901@gmail.com>
2021-08-14 17:34:28 +0530

    :sparkles: Finish app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
+import 'package:book_app/consttants.dart';
+import 'package:book_app/screens/home_screen.dart';
+import 'package:book_app/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/constants.dart';
-import 'package:myapp/screens/welcome_screem.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -16,12 +15,63 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         textTheme: Theme.of(context).textTheme.apply(
-          displayColor: kBlackColor,
-        )
+              displayColor: kBlackColor,
+            ),
       ),
       home: WelcomeScreen(),
     );
   }
 }
 
-
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Bitmap.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.headline2,
+                children: [
+                  TextSpan(
+                    text: "flamin",
+                  ),
+                  TextSpan(
+                    text: "go.",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              child: RoundedButton(
+                text: "start reading",
+                fontSize: 20,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return HomeScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
