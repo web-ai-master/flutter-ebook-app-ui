52b71db2e14100f4acd05ffcb6be39e65e20ab6d
jovan0901 <jovanvelickovic0901@gmail.com>
2021-08-14 09:12:30 +0530

    :sparkles: Add background image of Home page

--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -3,9 +3,24 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-
+        width: double.infinity,
+        decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/main_page_bg.png"),
+            alignment: Alignment.topCenter,
+            fit: BoxFit.fitWidth
+          )
+        ),
+        child: Column(
+          children:[
+            SizedBox(
+              height: size.height * .1,
+            ),
+          ]
+        ),
       )
     );
   }
